b42e259a22696a3dae3eec01bb1acef6aa9d1021
Colleen Murphy <colleen@gazlene.net>
2015-06-19 15:20:43 -0700

    Add variable to control OS

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,6 +7,7 @@ export DDD_WORKDIR=${DDD_WORKDIR:-"${HOME}/ddd-workdir"}
 export PATH=$PATH:$DDD_WORKDIR/tools/dib-dev-deploy/scripts
 export DDD_ELEMENTS_PATH=${DDD_ELEMENTS_PATH:-"${DDD_WORKDIR}/elements"}
 export DIB_HOSTNAME=${DIB_HOSTNAME:-'allinone'}
+export DIB_OS=${DIB_OS:-'ubuntu'}
 export DDD_VM_TEMPLATE=${DDD_VM_TEMPLATE:-"${DDD_WORKDIR}/templates/allinone-vm.xml"}
 export DDD_VM_MEMORY=${DDD_VM_MEMORY:-8192}
 export DDD_VM_CPUS=${DDD_VM_CPUS:-2}
@@ -26,7 +27,7 @@ ddd-pull-tools
 ### Build and install image
 
 image_path="${DDD_WORKDIR}/images/${DIB_HOSTNAME}.qcow2"
-ddd-create-image local-config ubuntu hostname puppet post-boot -o $image_path --image-size 40
+ddd-create-image $DIB_OS local-config hostname puppet post-boot -o $image_path --image-size 40
 ddd-define-vm $DIB_HOSTNAME $image_path
 virsh start $DIB_HOSTNAME
 echo "Waiting for vm to boot"
