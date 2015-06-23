ff8fab8094ecfdb017f5fbbf58d21785a2f087d7
Colleen Murphy <colleen@gazlene.net>
2015-06-23 00:12:30 -0700

    Remove post-boot element
    This element is no longer very useful. We can add ssh commands to the
    main script to accomplish things.

--- scripts/run.sh
+++ scripts/run.sh
@@ -33,7 +33,7 @@ ddd-pull-tools
 ### Build and install image
 
 image_path="${DDD_WORKDIR}/images/${DIB_HOSTNAME}.qcow2"
-ddd-create-image $DIB_OS dhcp-all-interfaces local-config puppet post-boot -o $image_path --image-size 40
+ddd-create-image $DIB_OS dhcp-all-interfaces local-config puppet -o $image_path --image-size 40
 ddd-define-vm $DIB_HOSTNAME $image_path
 virsh start $DIB_HOSTNAME
 set_hostname $DIB_HOSTNAME
